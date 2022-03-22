4c2cf42c4080fd86f12be52ad6c964c1281087e0
nggeokting <52147817+nggeokting@users.noreply.github.com>
2022-03-22 14:18:03 +0800

    Add files via upload

---
+++ Lab10Q3a.py
@@ -0,0 +1,13 @@
+# prompt user to enter module code
+code = input("Enter a module code: ")
+
+# use dict for a simple lookup as python does not have a switch-case construct
+switcher={
+    'CSC1001':'Introduction to Computing',
+    'CSC1008':'Data Structures and Algorithms',
+    'CSC1009':'Object-Oriented Programming',
+    'CSC1010':'Computer Networks'
+}
+
+# print out module name based on input code
+print(switcher[code])
