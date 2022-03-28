79889f03c5f12fc4f7f3139cd051c6eb8d3d6c92
nggeokting <52147817+nggeokting@users.noreply.github.com>
2022-03-28 16:44:47 +0800

    Delete Lab10Q2.py

--- Lab10Q2.py
+++
@@ -1,9 +0,0 @@
-# take in keyboard input for 2 numbers (x and y)
-x = input("Enter first number: ")
-y = input("Enter second number: ")
-
-# calculate average of the 2 numbers
-average = (float(x) + float(y)) / 2
-
-# print average of the 2 numbers
-print("The average of {x} and {y} is {average}".format(x=x, y=y, average=average))
